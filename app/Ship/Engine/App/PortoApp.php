f42f2587294427f02338b3385799e0c3122f4485
Mahmoud Zalt <mahmoud.z@msn.com>
2017-02-28 16:32:42 -0500

    just extend the Laravel Application

---
+++ app/Ship/Engine/App/PortoApp.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Ship\Engine\App;
+
+use Illuminate\Foundation\Application as laravelApplication;
+
+/**
+ * Extending Laravel Application Class
+ *
+ * Class PortoApp
+ *
+ * @author  Mahmoud Zalt  <mahmoud@zalt.me>
+ */
+class PortoApp extends laravelApplication
+{
+
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,7 @@
 |
 */
 
-$app = new Illuminate\Foundation\Application(
+$app = new App\Ship\Engine\App\PortoApp(
     realpath(__DIR__.'/../')
 );
 
