18643953a3a8a3aa235f94ba68142175097edccd
Mahmoud Zalt <mahmoud.z@msn.com>
2017-02-28 16:31:31 -0500

    replace Laravel http kernel with my ShipHttpKernel
    when creating the application from index.html

--- public/index.php
+++ public/index.php
@@ -47,7 +47,7 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 |
 */
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$kernel = $app->make(App\Ship\Engine\Kernels\ShipHttpKernel::class);
 
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
